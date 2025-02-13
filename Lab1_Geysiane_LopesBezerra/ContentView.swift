59ff0c1fa011868913dcd6b89d4cbee0ea5df9b5
geysianelopes <geysianelopes@hotmail.com>
2025-02-13 17:56:54 -0500

    implemented prime number checking function

--- Lab1_Geysiane_LopesBezerra/ContentView.swift
+++ Lab1_Geysiane_LopesBezerra/ContentView.swift
@@ -41,6 +41,14 @@ struct ContentView: View {
             }
         }
     }
+    
+    func isPrimeNumber(n: Int) -> Bool {
+        if n < 2 { return false }
+        for i in 2..<n {
+            if n % i == 0 { return false }
+        }
+        return true
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
