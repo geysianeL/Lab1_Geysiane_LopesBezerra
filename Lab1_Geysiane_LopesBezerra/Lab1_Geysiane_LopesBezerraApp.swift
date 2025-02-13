7a180495afee1dc69662b4d52a698b6adf8f42f7
geysianelopes <geysianelopes@hotmail.com>
2025-02-13 10:27:21 -0500

    Initial Commit

---
+++ Lab1_Geysiane_LopesBezerra/Lab1_Geysiane_LopesBezerraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Geysiane_LopesBezerraApp.swift
+//  Lab1_Geysiane_LopesBezerra
+//
+//  Created by Geysiane Lopes on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Geysiane_LopesBezerraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1_Geysiane_LopesBezerraTests/Lab1_Geysiane_LopesBezerraTests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Geysiane_LopesBezerraTests.swift
+//  Lab1_Geysiane_LopesBezerraTests
+//
+//  Created by Geysiane Lopes on 2025-02-13.
+//
+
+import Testing
+@testable import Lab1_Geysiane_LopesBezerra
+
+struct Lab1_Geysiane_LopesBezerraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
